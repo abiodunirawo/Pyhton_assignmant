48f9a906d6fc69861de0206b4aa4018b19bfbc20
Ogunbanwo Abiodun <78665281+abiodunirawo@users.noreply.github.com>
2023-03-21 19:00:17 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,23 @@
+name = "Skyline"
+age = 28
+
+
+print( "Enter name: skyline ")
+print("Enter age: 28"  )
+
+
+current_year = 2023
+
+def age_cal():
+    age_calculation = int(input("Enter year of Birth: "))
+    if age_calculation:
+        final_age = current_year - age_calculation
+    print("Hello Skyline your age is:", final_age, "year old")
+
+
+
+
+age_cal()
+
+
+  
